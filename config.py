d9271b7e3c8dddc694914d7a99b1bd71a3f85ada
Joby-John <joby432john@gmail.com>
2025-11-09 17:29:42 +0530

    updated config

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 import os
 
-MAINTENANCE_MODE = os.environ.get('MAINTENANCE_MODE', 'False').lower() == True
+# Read the ENV variable. 
+# If it's not set, default to 'False'.
+# .lower() == 'true' handles 'True', 'true', or 'TRUE'.
+MAINTENANCE_MODE = os.environ.get('MAINTENANCE_MODE', 'False').lower() == 'true'
